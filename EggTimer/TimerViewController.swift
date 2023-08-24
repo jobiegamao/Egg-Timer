84f92dc5960def369bfad2eb849829292410f9c6
May <jm.gamao@digitalspaceexplorer.com>
2023-08-24 15:37:49 +0800

    Create egg timer

---
+++ EggTimer/TimerViewController.swift
@@ -0,0 +1,87 @@
+//
+//  TimerViewController.swift
+//  EggTimer
+//
+//  Created by Squadzip on 8/24/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resumeButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    var timer: Timer = Timer()
+    var timerCount = 0 {
+        didSet {
+            if timerCount != 0 {
+                timerLabel.text = "\(timerCount) Seconds Left"
+            } else {
+                timerLabel.text = "Egg is \(eggButtonTapped) Cooked"
+                showDoneButton()
+            }
+            
+            let progress = 1  - (timerCount / eggButtonTapped.timer)
+            self.progressView.progress = Float(progress)
+        }
+    }
+    var eggButtonTapped: Egg = .Soft
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        onStartTimer()
+    }
+    
+    private func showDoneButton() {
+        pauseButton.isHidden = true
+        cancelButton.isHidden = true
+        doneButton.isHidden = false
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        self.dismiss(animated: true)
+    }
+    
+    private func onStartTimer() {
+        progressView.progress = 0
+        timer.invalidate()
+        timerCount = eggButtonTapped.timer
+        runTimer()
+    }
+    
+    private func runTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] timer in
+            guard let self = self else { return }
+            timerCount -= 1
+            print(timerCount)
+            if timerCount == 0 {
+                timer.invalidate()
+            }
+        })
+    }
+    
+    
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        self.dismiss(animated: true)
+    }
+    
+    @IBAction func pauseButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        sender.isHidden = true
+        resumeButton.isHidden = false
+    }
+    
+    @IBAction func resumeButtonTapped(_ sender: UIButton) {
+        runTimer()
+        sender.isHidden = true
+        pauseButton.isHidden = false
+    }
+}
--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    override func viewDidLoad() {
+         super.viewDidLoad()
+    }
+    
+    @IBAction func eggButtonTapped(_ sender: UIButton) {
+        if let buttonText = sender.currentTitle,
+            let eggButtonTapped = Egg(rawValue: buttonText) {
+    
+            let vc = storyboard?.instantiateViewController(withIdentifier: "TimerViewController") as! TimerViewController
+            vc.eggButtonTapped = eggButtonTapped
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: false)
+        }
+    }
+}
 
-
+enum Egg: String {
+    case Soft
+    case Medium
+    case Hard
+    
+    var timer: Int {
+        switch self {
+        case .Soft:
+            return 5
+        case .Medium:
+            return 8
+        case .Hard:
+            return 12
+        }
+    }
 }
